415acc0bfe3ad3b7a9cdfa0fa327b42f90fa833d
Artem <svar4evskya@yandex.ru>
2026-06-01 17:48:11 +0300

    lab 5-7

--- src/Document.cpp
+++ src/Document.cpp
@@ -1,7 +1,7 @@
 #include "Document.h"
 #include <utility>
-
-Document::Document(Id id, std::string name, std::string content, std::vector<std::string> tokens)
-    : id_(id), name_(std::move(name)), content_(std::move(content)), tokens_(std::move(tokens))
+using namespace std;
+Document::Document(Id id, string name, string content, vector<string> tokens)
+    : id_(id), name_(move(name)), content_(move(content)), tokens_(move(tokens))
 {
 }
